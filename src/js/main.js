22604499d55772b282809d50eafdaeff6225e688
Malorodova Anna <LAbsente@yandex.ru>
2021-06-27 00:33:17 +0300

    Mobile menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,13 @@
 import $ from '../local_modules/jquery/dist/jquery.min'
 
 $(document).ready(() => {
+    
     // eslint-disable-next-line no-console
     console.log(`document ready`)
 })
+
+document.querySelector('.menu-icon-wrapper').onclick = function(){
+    document.querySelector('.menu-icon').classList.toggle('menu-icon-active');
+    document.querySelector('.menu-icon-wrapper').classList.toggle('active');
+    document.querySelector('.toggle-menu').classList.toggle('active');
+}
